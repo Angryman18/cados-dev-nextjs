d2bbd2013572fd8baf5ec21ce413cacc4a7a80b1
yung-coder <chandel.sanjeev5@gmail.com>
2022-11-03 19:39:20 +0530

    fetch advocate

--- pages/advocate/[slug].tsx
+++ pages/advocate/[slug].tsx
@@ -2,11 +2,22 @@ import axios from "axios";
 import { requestToBodyStream } from "next/dist/server/body-streams";
 import { useRouter } from "next/router";
 import React, { useEffect } from "react";
+import { getAdvocate } from "../../service/advocates.api";
 
 const Advocate = () => {
   const router = useRouter();
-
-  return <div></div>;
+  const {slug} = router.query;
+  useEffect((): void => {
+    const getInfo = async (): Promise<any> => {
+      return await  getAdvocate(slug);
+    };
+    getInfo();
+    // console.log(slug);
+  }, []);
+  
+  return <div>
+     <button >Click</button>
+  </div>;
 };
 
 export default Advocate;
--- service/advocates.api.ts
+++ service/advocates.api.ts
@@ -1,10 +1,19 @@
 import axios from "axios";
 const backendURL: string = "https://cados.up.railway.app/advocates/";
-const advocateurl: string ='https://cados.up.railway.app/advocates/'
 
 export const getAdvocates = async (page: number): Promise<any> => {
   try {
     const response: any = await axios.get(`${backendURL}?page=${page}`);
+    return response.data;
+  } catch (err: unknown) {
+    return Promise.reject(err);
+  }
+};
+
+export const getAdvocate = async (username: string | string[] | undefined): Promise<any> => {
+  try {
+    const response: any = await axios.get(`${backendURL}${username} `);
+    console.log(response.data);
     return response.data;
   } catch (err: unknown) {
     return Promise.reject(err);
